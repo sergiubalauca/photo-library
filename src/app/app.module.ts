0214b03fb4ed8439fc3a761fbfa1323058e10f5c
Sergiu <sergiu.balauca@gmail.com>
2023-03-24 13:27:25 +0200

    added material ui and shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,8 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     BrowserModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
+      // enabled: !isDevMode(),
+      enabled: true,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
